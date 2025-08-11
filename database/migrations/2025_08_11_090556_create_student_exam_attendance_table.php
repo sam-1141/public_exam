d2aa1ee2144073654dee942cec1d6ab142b6bf0e
kh-almas <services.almas@gmail.com>
2025-08-11 16:05:38 +0600

    fix bug and make new migration

---
+++ database/migrations/2025_08_11_090556_create_student_exam_attendance_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_exam_attendance', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('student_id');
+            $table->bigInteger('exam_id');
+            $table->timestamp('exam_start_time');
+            $table->timestamp('exam_end_time')->nullable();
+            $table->timestamp('submit_time')->nullable();
+            $table->tinyInteger('submit_status')->comment('1=by_student, 2=time_expired, 3=forced_exit');
+            $table->integer('exam_total_mark')->default(0)->nullable();
+            $table->integer('student_total_mark')->default(0)->nullable();
+            $table->integer('total_correct_answers')->default(0)->nullable();
+            $table->integer('total_skipped_answers')->default(0)->nullable();
+            $table->integer('tab_switch_count')->default(0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_exam_attendance');
+    }
+};
