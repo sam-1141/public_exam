fe2a74b0da106982f3a3ed74d558116ec659c65c
kh-almas <services.almas@gmail.com>
2025-08-25 14:10:12 +0600

    dynamic student dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
@@ -12,7 +13,45 @@ class DashboardController extends Controller
          if($user->role == 'admin') {
             return Inertia::render('Admin/Dashboard/Dashboard');
         }else {
-            return Inertia::render('Student/Dashboard/Dashboard');
+             $userId = auth()->id();
+
+             $permittedCourses = DB::connection('Webapp')
+                 ->table('course_student')
+                 ->where('student_id', $userId)
+                 ->pluck('course_id')
+                 ->toArray();
+
+             $totalExam = DB::table('live_exams')
+                 ->where('exam_type', 0)
+                 ->whereIn('course_exam.course_id', $permittedCourses)
+                 ->join('course_exam', 'live_exams.id', '=', 'course_exam.exam_id')
+                 ->count();
+
+             $totalAttended = DB::table('student_exam_attendance')
+                 ->where('student_id', $userId)
+                 ->count();
+
+             $runningExams = DB::table('live_exams')
+                 ->where('exam_type', 0)
+                 ->where('start_time', '<', now())
+                 ->where('end_time', '>', now())
+                 ->whereIn('course_exam.course_id', $permittedCourses)
+                 ->join('course_exam', 'live_exams.id', '=', 'course_exam.exam_id')
+                 ->count();
+
+             $upcomingExams = DB::table('live_exams')
+                 ->where('exam_type', 0)
+                 ->where('start_time', '>', now())
+                 ->whereIn('course_exam.course_id', $permittedCourses)
+                 ->join('course_exam', 'live_exams.id', '=', 'course_exam.exam_id')
+                 ->count();
+
+            return Inertia::render('Student/Dashboard/Dashboard', [
+                'totalExam' => $totalExam,
+                'totalAttended' => $totalAttended,
+                'runningExams' => $runningExams,
+                'upcomingExams' => $upcomingExams,
+            ]);
         }
     }
 
